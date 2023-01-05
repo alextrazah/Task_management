a61438d3a681fc005a741f7c645ccad543d39d1e
AzizSYW <mohamedaziz.sahnoun@esprit.tn>
2023-01-06 00:32:37 +0100

    task

--- Story.js
+++ Story.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './Story.css'
 
 function Story( { image, profileSrc, title }) {
-    image = "http://www.skiesbook.com:3000/uploads/pexels-cottonbro-7450291-1658143025794.jpg"
+    image = process.env.REACT_APP_API_IMAGE +"/pexels-cottonbro-7450291-1658143025794.jpg"
     return (
         <div style = {{ backgroundImage : `url(${image})`, backgroundRepeat: "no-repeat"}} className = "story">
         </div>
